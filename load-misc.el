c75fec6fa58b40d9859679182afb2ac2cce7374b
Martin Luder <maser@winterfell.ch>
2010-01-19 22:41:31 +0100

    Better ido key bindings, lorem function

--- load-misc.el
+++ load-misc.el
@@ -48,6 +48,14 @@
 ;; Interactively Do Things (highly recommended, but not strictly required)
 (require 'ido)
 (ido-mode t)
+(add-hook 'ido-setup-hook '(lambda ()
+			     (define-key ido-completion-map "\C-h" 'ido-delete-backward-updir)
+			     (define-key ido-completion-map "\C-n" 'ido-next-match)
+			     (define-key ido-completion-map "\C-f" 'ido-next-match)
+			     (define-key ido-completion-map "\C-p" 'ido-prev-match)
+			     (define-key ido-completion-map "\C-b" 'ido-prev-match)
+			     (define-key ido-completion-map " " 'ido-exit-minibuffer)
+	  ))
 
 ;; flymake
 (require 'flymake)
@@ -100,3 +108,15 @@
 
 (add-to-list 'load-path "~/.emacs.d/git-emacs")
 (require 'git-emacs)
+
+;; Lorem ipsum dolor sit amet
+(defun lorem ()
+  "Insert a lorem ipsum."
+  (interactive)
+  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+"eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad "
+"minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+"aliquip ex ea commodo consequat. Duis aute irure dolor in "
+"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+"culpa qui officia deserunt mollit anim id est laborum."))
