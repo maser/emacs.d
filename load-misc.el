e081c2949b71bc903c3bd9e4d8d2fc8e6a02797c
Martin Luder <maser@winterfell.ch>
2010-03-25 08:26:29 +0100

    added sml-modeline.el

--- load-misc.el
+++ load-misc.el
@@ -118,3 +118,10 @@
 "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
 "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
 "culpa qui officia deserunt mollit anim id est laborum."))
+
+(if (require 'sml-modeline nil 'noerror)    ;; use sml-modeline if available
+  (progn 
+    (sml-modeline-mode 1)                   ;; show buffer pos in the mode line
+    (scroll-bar-mode -1))                   ;; turn off the scrollbar
+  (scroll-bar-mode 1)                       ;; otherwise, show a scrollbar...
+  (set-scroll-bar-mode 'right))             ;; ... on the right
