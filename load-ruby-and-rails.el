129fd12ad3d55337d4a512c1f1653bcdc7074af0
Martin Luder <maser@winterfell.ch>
2010-05-10 13:04:01 +0200

    cleaned up rsense comment and removed ac-complete-rsense shortcut I never use

--- load-ruby-and-rails.el
+++ load-ruby-and-rails.el
@@ -16,7 +16,6 @@
            (local-set-key (kbd "<return>") 'newline-and-indent)
 	   (add-to-list 'ac-sources 'ac-source-rsense-method)
 	   (add-to-list 'ac-sources 'ac-source-rsense-constant)
-	   (local-set-key (kbd "C-c .") 'ac-complete-rsense)
 ))
 
 (load-file "~/.emacs.d/flymake-ruby.el")
@@ -38,13 +37,7 @@
 ;; (require 'rdebug)
 ;; (setq rdebug-short-key-mode t)
 
-;; Replace $RSENSE_HOME with the directory where RSense was installed in full path
-;; Example for UNIX-like systems
-;; (setq rsense-home "/home/tomo/opt/rsense-0.2")
-;; or
-;; (setq rsense-home (expand-file-name "~/opt/rsense-0.2"))
-;; Example for Windows
-;; (setq rsense-home "C:\\rsense-0.2")
+;; Rsense
 (setq rsense-home "/home/maser/.emacs.d/rsense")
 (add-to-list 'load-path (concat rsense-home "/etc"))
 (require 'rsense)
