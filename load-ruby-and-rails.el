b56d16435f11a0a03fc66cdaa8630e030d73b00d
Martin Luder <maser@winterfell.ch>
2010-12-03 20:52:55 +0100

    untabify ruby files on write

--- load-ruby-and-rails.el
+++ load-ruby-and-rails.el
@@ -11,6 +11,10 @@
 ;; ruby-mode-hook
 (add-hook 'ruby-mode-hook
          (lambda ()
+	   (add-hook 'local-write-file-hooks '(lambda ()
+						(save-excursion
+						  (untabify (point-min) (point-max))
+						  )))
            (set (make-local-variable 'indent-tabs-mode) 'nil)
            (set (make-local-variable 'tab-width) 2)
            ;; (imenu-add-to-menubar "IMENU")
