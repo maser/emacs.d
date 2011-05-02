9f9f95f5552ee06f2cd9e9b338c01721ac13727d
Martin Luder <maser@winterfell.ch>
2011-05-02 22:37:13 +0200

    I never want scrollbars

--- load-misc.el
+++ load-misc.el
@@ -90,9 +90,7 @@
 (if (require 'sml-modeline nil 'noerror)    ;; use sml-modeline if available
   (progn
     (sml-modeline-mode 1)                   ;; show buffer pos in the mode line
-    (scroll-bar-mode -1))                   ;; turn off the scrollbar
-  (scroll-bar-mode 1)                       ;; otherwise, show a scrollbar...
-  (set-scroll-bar-mode 'right))             ;; ... on the right
+    (scroll-bar-mode -1)))                  ;; turn off the scrollbar
 
 ;; org-mode
 (require 'org-install)
