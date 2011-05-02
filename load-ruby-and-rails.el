57293fea2bf49e4975a9d8e5bd420deeae6d7675
Martin Luder <maser@winterfell.ch>
2011-05-02 22:16:52 +0200

    updated load-ruby-and-rails.el

--- load-ruby-and-rails.el
+++ load-ruby-and-rails.el
@@ -20,17 +20,24 @@
 ;; ruby-mode-hook
 (add-hook 'ruby-mode-hook
          (lambda ()
-	   (add-hook 'write-file-functions '(lambda ()
-					      (save-excursion
-						(untabify (point-min) (point-max))
-						)))
+           (add-hook 'after-save-hook '(lambda ()
+                                         (when (and (eq major-mode 'ruby-mode) buffer-file-name (not (string-match "\.erb$" buffer-file-name)))
+                                           (if tags-file-name
+                                               (let* ((file-to-update buffer-file-name)
+                                                      (tags-file-to-update tags-file-name)
+                                                      (output (
+                                                               (condition-case err
+								   (with-output-to-string
+								     (with-current-buffer standard-output
+								       (call-process "rtags" nil t nil "-a" "-f" tags-file-to-update file-to-update)))
+								 (error (message "%s" (error-message-string err)))))))
+                                                 (message output))))))
            (set (make-local-variable 'indent-tabs-mode) 'nil)
            (set (make-local-variable 'tab-width) 2)
-           ;; (imenu-add-to-menubar "IMENU")
            (local-set-key (kbd "<return>") 'my-newline-and-indent)
-	   (add-to-list 'ac-sources 'ac-source-rsense-method)
-	   (add-to-list 'ac-sources 'ac-source-rsense-constant)
-	   (flyspell-prog-mode)
+           (add-to-list 'ac-sources 'ac-source-rsense-method)
+           (add-to-list 'ac-sources 'ac-source-rsense-constant)
+           (flyspell-prog-mode)
 ))
 
 (load-file "~/.emacs.d/flymake-ruby.el")
