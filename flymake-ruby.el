4aadda669a2198dac8af4b539d12680f5512ceec
Martin Luder <maser@winterfell.ch>
2011-05-02 21:29:07 +0200

    stop random flymake-mode switching

--- flymake-ruby.el
+++ flymake-ruby.el
@@ -8,7 +8,7 @@
 (defun flymake-ruby-init ()
   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
-	 (local-file  (file-relative-name
+         (local-file  (file-relative-name
                        temp-file
                        (file-name-directory buffer-file-name))))
     (list "ruby" (list "-c" local-file))))
@@ -21,7 +21,7 @@
 (add-hook 'ruby-mode-hook
           '(lambda ()
 
-	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-		 (flymake-mode))
-	     ))
+             ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+             (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+                 (flymake-mode t))
+             ))
