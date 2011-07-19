b77e1b7aca291bd173ffa1879618bb060ca1631c
Martin Luder <maser@winterfell.ch>
2011-07-19 09:18:40 +0200

    Viper configuration (disabled)

--- load-misc.el
+++ load-misc.el
@@ -132,3 +132,8 @@
     (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
     (add-hook 'scss-mode-hook 'rainbow-delimiters-mode)
     (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)))
+
+;; (setq viper-mode nil)
+;; (setq viper-expert-level  '5)
+;; (setq viper-inhibit-startup-message  't)
+;; (require 'viper)
