4a8a88eb968d3d6b524df6c2faf958f91bd199c9
Martin Luder <maser@winterfell.ch>
2011-07-19 09:17:55 +0200

    rainbow-delimiters

--- load-misc.el
+++ load-misc.el
@@ -125,3 +125,10 @@
 
 (if load-zeitgeist
     (load-file "~/.emacs.d/zeitgeist.el"))
+
+(when (require 'rainbow-delimiters nil 'noerror)
+  (progn
+    (add-hook 'ruby-mode-hook 'rainbow-delimiters-mode)
+    (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
+    (add-hook 'scss-mode-hook 'rainbow-delimiters-mode)
+    (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)))
