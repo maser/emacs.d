12a8c344fded732d2a53f7a06d5f5cd17928ee37
Martin Luder <maser@winterfell.ch>
2011-11-06 00:50:29 +0100

    Sane indentation for coffee-mode

--- coffee-mode.el
+++ coffee-mode.el
@@ -1,3 +1,5 @@
 ;; coffee-mode
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
 (require 'coffee-mode)
+(add-hook 'coffee-mode-hook
+	  '(lambda() (set (make-local-variable 'tab-width) 2)))
