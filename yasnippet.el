ddff788ba009f950cb4480a75791c8e3fca9bd47
Martin Luder <maser@winterfell.ch>
2011-11-06 00:10:23 +0100

    Install real yasnippet, not strange yasnippet-bundle

--- yasnippet.el
+++ yasnippet.el
@@ -1,1 +1,4 @@
-(require 'yasnippet-bundle)
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet-0.6.1c/")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
