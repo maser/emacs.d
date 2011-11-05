053344dd2bf430a25ca4511f1e58f1cc96ddcecb
Martin Luder <maser@winterfell.ch>
2011-11-06 00:23:07 +0100

    Change org-mode configuration
    - Add flyspell
    - Map C-c a to org-agenda
    - Lazy TODOs: ustom agenda command for todos without time constraints

--- org.el
+++ org.el
@@ -1,4 +1,10 @@
 ;; org-mode
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(setq org-log-done t)
+(add-hook 'org-mode-hook 'flyspell-mode)
+(global-set-key "\C-ca" 'org-agenda)
+
+(setq org-agenda-custom-commands
+      '(("l" "lazy TODOs" todo nil
+	 ((org-agenda-skip-function
+	  '(org-agenda-skip-entry-if 'scheduled 'deadline 'timestamp))))))
