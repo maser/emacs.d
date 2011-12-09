64e3ea45f90355a25c4d7ab7dbbfeae9e9f8d1a1
Martin Luder <maser@winterfell.ch>
2011-12-09 15:41:32 +0100

    Map C-w to backward-kill-word, remap kill-region to C-x C-k

--- basic-customization.el
+++ basic-customization.el
@@ -60,3 +60,7 @@
 
 ;; dired
 (setq dired-listing-switches "-aohD")
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
