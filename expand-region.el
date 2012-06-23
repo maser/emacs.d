ce059b9032129dd440307149ee84685528dac5af
Martin Luder <maser@winterfell.ch>
2012-06-23 18:13:51 +0200

    expand-region: Change shortcut to C-M-m (was C-@)

--- expand-region.el
+++ expand-region.el
@@ -1,3 +1,3 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/expand-region.el")
 (require 'expand-region)
-(global-set-key (kbd "C-@") 'er/expand-region)
+(global-set-key (kbd "C-M-m") 'er/expand-region)
