d7ae6a7d96c060e639a665f00872bc7aad86d786
Martin Luder <maser@winterfell.ch>
2012-06-23 21:12:34 +0200

    Small ido adjustments

--- ido.el
+++ ido.el
@@ -1,6 +1,13 @@
 ;; Interactively Do Things
 (require 'ido)
 (ido-mode t)
+
+(setq ido-enable-flex-matching t
+      ido-enable-prefix nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point nil
+      ido-max-prospects 10)
+
 (add-hook 'ido-setup-hook '(lambda ()
 			     (define-key ido-completion-map "\C-h" 'ido-delete-backward-updir)
 			     (define-key ido-completion-map "\C-n" 'ido-next-match)
@@ -9,6 +16,11 @@
 			     (define-key ido-completion-map "\C-b" 'ido-prev-match)
 			     (define-key ido-completion-map " " 'ido-exit-minibuffer)
 	  ))
+
+;; Use C-w to go back up a dir to better match normal usage of C-w
+;; - insert current file name with C-x C-w instead.
+(define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
+(define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)
 
 ;; Display ido results vertically, rather than horizontally
 (setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
