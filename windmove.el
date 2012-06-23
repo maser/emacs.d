0597cd133cc3ba7c0db1174fa2f014174ebc4d76
Martin Luder <maser@winterfell.ch>
2012-06-23 18:26:16 +0200

    Stop windmove from showing stacktraces when moving is not possible

--- windmove.el
+++ windmove.el
@@ -1,5 +1,21 @@
 ;; windmove
-(global-set-key (kbd "M-N") 'windmove-down)
-(global-set-key (kbd "M-P") 'windmove-up)
-(global-set-key (kbd "M-F") 'windmove-right)
-(global-set-key (kbd "M-B") 'windmove-left)
+
+; windmove shows a stacktrace when there is nothing to move to
+
+(defmacro maser/swallow-errors (name f-with-error)
+  `(defun ,name ()
+  (interactive)
+  (condition-case err
+       (,f-with-error)
+     (error
+      (message "%s" (error-message-string err))))))
+
+(maser/swallow-errors windmove-down-without-errors  windmove-down)
+(maser/swallow-errors windmove-up-without-errors  windmove-up)
+(maser/swallow-errors windmove-right-without-errors  windmove-right)
+(maser/swallow-errors windmove-left-without-errors  windmove-left)
+
+(global-set-key (kbd "M-N") 'windmove-down-without-errors)
+(global-set-key (kbd "M-P") 'windmove-up-without-errors)
+(global-set-key (kbd "M-F") 'windmove-right-without-errors)
+(global-set-key (kbd "M-B") 'windmove-left-without-errors)
