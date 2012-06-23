77626db5cc89c4d45dfeebc7e8fb49ae634bf1c5
Martin Luder <maser@winterfell.ch>
2012-06-23 18:10:01 +0200

    dired: Hide file details, nicer C-a

--- dired-details.el
+++ dired-details.el
@@ -1,3 +1,11 @@
 ;; dired-details
 (require 'dired-details)
-(setq dired-details-hidden-string "")
+(setq dired-details-hidden-string "--- ")
+(dired-details-install)
+
+;; C-a is nicer in dired if it moves back to start of files
+(defun dired-back-to-start-of-files ()
+  (interactive)
+  (backward-char (- (current-column) 2)))
+
+(define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
