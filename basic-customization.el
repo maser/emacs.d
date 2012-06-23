82b0aa0582d116652f03b2082a1be400ee5aaa9b
Martin Luder <maser@winterfell.ch>
2012-06-23 22:27:02 +0200

    Map C-h to backspace, F1 to help

--- basic-customization.el
+++ basic-customization.el
@@ -77,3 +77,7 @@
 ;; volatile-highlights
 (require 'volatile-highlights)
 (volatile-highlights-mode t)
+
+;; Use shell-like backspace C-h, rebind help to F1
+(define-key key-translation-map [?\C-h] [?\C-?])
+(global-set-key (kbd "<f1>") 'help-command)
