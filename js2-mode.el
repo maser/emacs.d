8cc0f8b7674d9f3b85926884f7e6a263d3221017
Martin Luder <maser@winterfell.ch>
2012-06-23 18:16:02 +0200

    js2-mode: indent 4 spaces

--- js2-mode.el
+++ js2-mode.el
@@ -5,51 +5,15 @@
 (autoload 'espresso-mode "espresso")
 
 ;; js2 configuration
-(setq js-indent-level 2)
-(setq js2-basic-offset 2)
+(setq js-indent-level 4)
+(setq js2-basic-offset 4)
 (setq js2-mode-indent-ignore-first-tab t)
-(setq espresso-indent-level 2)
-(setq c-basic-offset 2)
-
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (espresso--proper-indentation parse-status))
-           node)
-
-      (save-excursion
-
-        ;; I like to indent case and labels to half of the tab width
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-              (setq indentation (+ indentation (/ espresso-indent-level 2))))
-
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;;     bar = "foo";
-        (message "testing for var node")
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (message "var node detected!")
-          (setq indentation (+ 4 indentation)))
-        )
-
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
+(setq espresso-indent-level 4)
+(setq c-basic-offset 4)
 
 (defun my-js2-mode-hook ()
-  (require 'espresso)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  (define-key js2-mode-map [(return)] 'newline-and-indent)
+  (local-set-key (kbd "<return>") 'newline-and-indent)
   (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
-)
+      (js2-highlight-vars-mode)))
+
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
