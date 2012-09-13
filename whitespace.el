83291d70d5e661ff5ae057838e631c02d6097bc1
Martin Luder <maser@winterfell.ch>
2012-09-13 18:39:02 +0200

    global whitespace mode kills ERC faces, selectively enabling it in some modes

--- whitespace.el
+++ whitespace.el
@@ -1,3 +1,20 @@
 (require 'whitespace)
 (setq whitespace-style '(trailing face tabs lines-tail))
-(global-whitespace-mode)
+
+(defun enable-whitespace-mode ()
+  (whitespace-mode 1))
+(add-hook 'prog-mode-hook 'enable-whitespace-mode)
+
+(setq modes-where-I-want-whitespace-mode-to-be-enabled
+      '(ruby-mode-hook
+        javascript-mode-hook
+        js-mode-hook
+        css-mode-hook
+        sass-mode-hook
+        yaml-mode-hook
+        emacs-lisp-mode-hook
+        nxhtml-mode-hook))
+
+(mapc (lambda (mode-hook)
+        (add-hook mode-hook 'enable-whitespace-mode))
+      modes-where-I-want-whitespace-mode-to-be-enabled)
