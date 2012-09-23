e157523b388b1771aee78910cc238b98a2f08c6e
Martin Luder <maser@winterfell.ch>
2012-09-23 22:22:17 +0200

    Untabify packages.el

--- packages.el
+++ packages.el
@@ -5,39 +5,39 @@
 (package-initialize)
 
 (setq maser-required-packages (list
-			       'magit
-			       'auto-complete
-			       'coffee-mode
-			       'feature-mode
-			       'deft
-			       'dired-details
-			       'expand-region
-			       'flymake-cursor
-			       'haml-mode
-			       'js2-mode
+                               'magit
+                               'auto-complete
+                               'coffee-mode
+                               'feature-mode
+                               'deft
+                               'dired-details
+                               'expand-region
+                               'flymake-cursor
+                               'haml-mode
+                               'js2-mode
                                'flymake-jshint
-			       'mark-multiple
-			       'mark-more-like-this
-			       'maxframe
-			       'puppet-mode
-			       'rainbow-delimiters
-			       'rspec-mode
-			       'rvm
-			       'sass-mode
-			       'scss-mode
-			       'sml-modeline
-			       'undo-tree
-			       'zenburn-theme
-			       'yasnippet
-			       'crontab-mode
-			       'csv-mode
-			       'ruby-end
-			       'smex
-			       'volatile-highlights
-			       'ido-ubiquitous
-			       'flymake-ruby
+                               'mark-multiple
+                               'mark-more-like-this
+                               'maxframe
+                               'puppet-mode
+                               'rainbow-delimiters
+                               'rspec-mode
+                               'rvm
+                               'sass-mode
+                               'scss-mode
+                               'sml-modeline
+                               'undo-tree
+                               'zenburn-theme
+                               'yasnippet
+                               'crontab-mode
+                               'csv-mode
+                               'ruby-end
+                               'smex
+                               'volatile-highlights
+                               'ido-ubiquitous
+                               'flymake-ruby
                                'yaml-mode
-			       'flymake-sass))
+                               'flymake-sass))
 
 (dolist (package maser-required-packages)
   (when (not (package-installed-p package))
