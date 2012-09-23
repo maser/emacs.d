84764e148c2d37da42973b08795e47fe0ff4eea3
Martin Luder <maser@winterfell.ch>
2012-09-23 22:23:00 +0200

    Make electric-indent-mode do nothing for sass-mode
    Automatic indentation in Sass is not a good idea.

--- sass-mode.el
+++ sass-mode.el
@@ -1,1 +1,6 @@
 (require 'sass-mode)
+
+(defun sass-electric-indent-function (char)
+  (if (eq major-mode 'sass-mode)
+      false))
+(add-hook 'electric-indent-functions 'sass-electric-indent-function)
