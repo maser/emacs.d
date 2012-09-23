6871360bbcfd1b4e19b42ffc6d5865e5a97acf51
Martin Luder <maser@winterfell.ch>
2012-09-23 22:17:21 +0200

    Add C-M-c shortcut for contract-region (opposite of C-M-m for expand-region)

--- expand-region.el
+++ expand-region.el
@@ -1,3 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/expand-region.el")
 (require 'expand-region)
 (global-set-key (kbd "C-M-m") 'er/expand-region)
+(global-set-key (kbd "C-M-c") 'er/contract-region)
