ad78150d067659dab2f9e73f078d710e78c3095e
Martin Luder <maser@winterfell.ch>
2012-10-06 15:27:58 +0200

    Get org-mode from the official org elpa

--- packages.el
+++ packages.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
 (package-initialize)
 
@@ -10,6 +11,8 @@
                                'coffee-mode
                                'feature-mode
                                'deft
+                               'org
+                               'org-plus-contrib
                                'dired-details
                                'expand-region
                                'flymake-cursor
