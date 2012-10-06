c7f5d01c2d02d0665b635e05e13a9f893004a5f6
Martin Luder <maser@winterfell.ch>
2012-10-06 15:38:20 +0200

    Autocomplete css properties in sass/scss modes

--- sass-mode.el
+++ sass-mode.el
@@ -4,3 +4,8 @@
   (if (eq major-mode 'sass-mode)
       false))
 (add-hook 'electric-indent-functions 'sass-electric-indent-function)
+
+(defun configure-auto-complete-for-sass ()
+  (add-to-list 'ac-sources 'ac-source-css-property))
+(add-hook 'sass-mode-hook 'configure-auto-complete-for-sass)
+(add-to-list 'ac-modes 'sass-mode)
--- scss-mode.el
+++ scss-mode.el
@@ -1,3 +1,8 @@
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 (setq scss-compile-at-save nil)
+
+(defun configure-auto-complete-for-scss ()
+  (add-to-list 'ac-sources 'ac-source-css-property))
+(add-hook 'scss-mode-hook 'configure-auto-complete-for-scss)
+(add-to-list 'ac-modes 'scss-mode)
