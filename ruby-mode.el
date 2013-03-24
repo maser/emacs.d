0db83cdb5f465473b16aa18020f739549b30d95e
Martin Luder <maser@winterfell.ch>
2013-03-24 01:15:55 +0100

    Fix indentation in Ruby mode

--- ruby-mode.el
+++ ruby-mode.el
@@ -14,6 +14,22 @@
 (add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rabl$" . ruby-mode))
 
+(setq ruby-deep-indent-paren nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 (defun my-newline-and-indent ()
   "I don't like backtraces popping up and ruby-indent-line seems to really enjoy that."
   (interactive)
