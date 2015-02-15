3de106c32067d06b2db3d69a836a7fefc148a730
Martin Luder <maser@winterfell.ch>
2015-02-15 20:33:23 +0100

    Update yasnippet config

--- yasnippet.el
+++ yasnippet.el
@@ -2,3 +2,24 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/vendor/my-snippets")
+(setq yas-snippet-dirs (remove "~/.emacs.d/snippets" yas-snippet-dirs))
+
+(require 'popup)
+(defun yas/popup-isearch-prompt (prompt choices &optional display-fn)
+  (when (featurep 'popup)
+    (popup-menu*
+     (mapcar
+      (lambda (choice)
+        (popup-make-item
+         (or (and display-fn (funcall display-fn choice))
+             choice)
+         :value choice))
+      choices)
+     :prompt prompt
+     ;; start isearch mode immediately
+     :isearch t
+     )))
+
+(setq yas/prompt-functions
+      '(yas/popup-isearch-prompt
+        yas/ido-prompt))
