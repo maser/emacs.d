fc867a92addb87ea9f5d9eae42398ed6ecd5d2e1
Martin Luder <maser@winterfell.ch>
2018-03-07 00:05:52 +0100

    Update sass conf

--- sass-mode.el
+++ sass-mode.el
@@ -1,8 +1,3 @@
 (require 'sass-mode)
 
-(defun sass-electric-indent-function (char)
-  (if (eq major-mode 'sass-mode)
-      false))
-(add-hook 'electric-indent-functions 'sass-electric-indent-function)
-
 (add-hook 'sass-mode-hook 'rainbow-mode)
