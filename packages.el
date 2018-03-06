a321efc99a7e9d277dc26ace41fe8b3ee696a337
Martin Luder <maser@winterfell.ch>
2018-03-06 23:58:24 +0100

    Update package archive definitions

--- packages.el
+++ packages.el
@@ -1,10 +1,11 @@
 (require 'package)
-(setq package-archives '())
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(setq package-archive-priorities '(("melpa-stable" . 1)))
 
 (package-initialize)
 
