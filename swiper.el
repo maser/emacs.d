b9b9558e4fca27c5c9f977cedd72e2a5c84e36ec
Martin Luder <maser@winterfell.ch>
2018-03-07 00:48:26 +0100

    Convert swiper conf to use-package

--- swiper.el
+++ swiper.el
@@ -1,14 +1,20 @@
 (use-package ivy
-  :ensure t)
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume))
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
 (use-package swiper
-  :ensure t)
+  :ensure t
+  :after ivy
+  :config
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key "\C-r" 'swiper))
+
 (use-package counsel
-  :ensure t)
-(counsel-mode 1)
-(global-set-key "\C-s" 'swiper)
-(global-set-key "\C-r" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
+  :ensure t
+  :after (swiper ivy)
+  :config
+  (counsel-mode 1))
