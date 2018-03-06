f6c58f64ef74e823c4c3c529f48a6e06f6dc7ec1
Martin Luder <maser@winterfell.ch>
2018-03-07 00:05:34 +0100

    Update company conf

--- company-mode.el
+++ company-mode.el
@@ -1,17 +1,7 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-
-(require 'color)
-
-(defun set-company-mode-face ()
-  (let ((bg (face-attribute 'default :background)))
-    (custom-set-faces
-     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-     `(company-tooltip-common ((t (:inherit font-lock-constant-face)))))))
-
-;; (set-company-mode-face)
-
 (company-quickhelp-mode 1)
+
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
