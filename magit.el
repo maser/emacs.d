8b24fe17e1f046af7bcf23ed4614975f7f31b047
Martin Luder <maser@winterfell.ch>
2020-04-15 16:43:14 +0200

    Add magit forge

--- magit.el
+++ magit.el
@@ -3,3 +3,12 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 (setq magit-completing-read-function 'ivy-completing-read)
+
+(use-package glab
+  :ensure t)
+
+(use-package forge
+  :ensure t
+  :after (magit glab)
+  :config
+  (add-to-list 'forge-alist '("gitlab.cognita.ch" "gitlab.cognita.ch/api/v4" "gitlab.cognita.ch" forge-gitlab-repository)))
