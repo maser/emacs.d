896a1155b2f4bba89e92bef2a147d67bdb137d9c
Martin Luder <maser@winterfell.ch>
2020-04-15 16:41:26 +0200

    Remove some unnecessary ido config

--- ido.el
+++ ido.el
@@ -9,18 +9,13 @@
       ido-max-prospects 10)
 
 (add-hook 'ido-setup-hook '(lambda ()
-			     (define-key ido-completion-map "\C-h" 'ido-delete-backward-updir)
-			     (define-key ido-completion-map "\C-n" 'ido-next-match)
-			     (define-key ido-completion-map "\C-f" 'ido-next-match)
-			     (define-key ido-completion-map "\C-p" 'ido-prev-match)
-			     (define-key ido-completion-map "\C-b" 'ido-prev-match)
-			     (define-key ido-completion-map " " 'ido-exit-minibuffer)
-	  ))
-
-;; Use C-w to go back up a dir to better match normal usage of C-w
-;; - insert current file name with C-x C-w instead.
-(define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
-(define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)
+                             (define-key ido-completion-map "\C-h" 'ido-delete-backward-updir)
+                             (define-key ido-completion-map "\C-n" 'ido-next-match)
+                             (define-key ido-completion-map "\C-f" 'ido-next-match)
+                             (define-key ido-completion-map "\C-p" 'ido-prev-match)
+                             (define-key ido-completion-map "\C-b" 'ido-prev-match)
+                             (define-key ido-completion-map " " 'ido-exit-minibuffer)
+          ))
 
 (use-package ido-ubiquitous
   :ensure t)
